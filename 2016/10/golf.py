b574d91c6cb77f3eef21fbe716b24c98774fcbcb
SkocicD <skocicdavid@gmail.com>
2026-02-03 01:56:02 -0500

    416

--- 2016/10/golf.py
+++ 2016/10/golf.py
@@ -7,22 +7,16 @@ for l in open(0):
  k=[int(x)for x in z if x[0]<'a']
  if len(k)<3:
   v,b=k
-  if len(s[b]):
-   o(q,b)
+  if len(s[b]):o(q,b)
   o(s[b],v)
- else:
-  a=('b'in z[5],k[1])
-  b=('b'in z[-2],k[2])
-  g[k[0]]=[b,a]if 'h'in z[3] else[a,b]
+ else:a=('b'in z[5],k[1]);b=('b'in z[-2],k[2]);g[k[0]]=[b,a]if 'h'in z[3] else[a,b]
 p=1
 while q:
  w=q.pop()
  if [17,61]==(z:=sorted(s[w])):print(w)
  for(a,b),x in zip(g[w],z):
   if a:
-   if len(s[b]):
-    o(q,b)
+   if len(s[b]):o(q,b)
    o(s[b],x)
-  elif b<3:
-   p*=x
+  elif b<3:p*=x
 print(p)
